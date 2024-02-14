ec2ebe9d0aa3325493add344ae8376d9bb862574
Aditya Karad <ankarad@uci.edu>
2024-02-14 14:52:55 -0800

    corrected

--- src/main/java/com/karad/k8s/controller/StudentController.java
+++ src/main/java/com/karad/k8s/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.karad.k8s.controller;
 
+import com.karad.k8s.K8sApplication;
 import com.karad.k8s.model.Student;
 import com.karad.k8s.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,11 +14,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class StudentController {
-
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
     private final StudentService studentsService;
 
     @GetMapping("/")
     public List<Student> getAllStudents() {
+        System.out.println("Printing jdbcUrl: " + jdbcUrl);
         return studentsService.getAllStudents();
     }
 
--- src/main/java/com/karad/k8s/repository/StudentRepository.java
+++ src/main/java/com/karad/k8s/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package com.karad.k8s.repository;
 
 import com.karad.k8s.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
